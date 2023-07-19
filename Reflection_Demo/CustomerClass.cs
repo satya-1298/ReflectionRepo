c4171420d484c159f737cc419ed659ee80836359
unknown <satya-1298>
2023-07-19 09:47:00 +0530

    [Satya]Add : Describes the data of given data

---
+++ Reflection_Demo/CustomerClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Demo
+{
+    public class CustomerClass
+    {
+        public int a, b;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CustomerClass()
+        {
+            Id= 1;
+            Name = "Satya";
+        }
+        public CustomerClass(int id)
+        {
+            Id= id;
+        }
+        public void Display()
+        {
+
+        }
+        public void Update(int d)
+        {
+
+        }
+    }
+}
---
+++ Reflection_Demo/Reflection_Problem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Demo
+{
+    //Reflection - It allows to infect or a information of class constructor method and 
+    // field at a run time
+    //Reflection is a process of describing the metadata of a method , constructor, fields etc..,
+    //Meta data referce to a binary information saved in a memory
+    public class Reflection_Problem
+    {
+        public static void DataInfo()
+        {
+            
+            Type type = Type.GetType("Reflection_Demo.CustomerClass");
+            Console.WriteLine(type.FullName);
+            Console.WriteLine(type.Name);
+            Console.WriteLine(type.Namespace);
+
+            PropertyInfo[] properties = type.GetProperties();
+            foreach(PropertyInfo info in properties)
+            {
+                Console.WriteLine(info.PropertyType.Name+" "+info.Name);
+            }
+            Console.WriteLine("\n");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach(ConstructorInfo constructor in constructors) 
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+            Console.WriteLine("\n");
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach(MethodInfo info in methods)
+            {
+                Console.WriteLine(info.ReturnType.Name+" "+info.Name);
+            }
+        }
+    }
+}
